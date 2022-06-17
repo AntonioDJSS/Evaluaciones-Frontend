394d921e6d266cc152e55873606f8d2f736cd754
AntonioDJSS <antoniodjss2000@gmail.com>
2022-06-16 23:44:55 -0500

    Correccion de errores de deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className="rotate flex justify-center">
-<img className="w-60 px-20 py-6 logo"></img>
+<img className="w-60 px-20 py-6" src="https://i.imgur.com/RbRUeTh.png"></img>
 </div>
   )
 }
