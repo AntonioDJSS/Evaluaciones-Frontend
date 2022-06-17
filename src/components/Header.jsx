0881ffa47b40f269f4cbe97bb75f22c6c24a9ad1
AntonioDJSS <antoniodjss2000@gmail.com>
2022-06-17 15:09:05 -0500

    Deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className="rotate flex justify-center">
-<img className="w-96 px-10 py-6" src="../src/assets/LOGO.svg"></img>
+<img className="w-96 px-10 py-6" src="https://imgur.com/uLXGWB5.png"></img>
 </div>
   )
 }
