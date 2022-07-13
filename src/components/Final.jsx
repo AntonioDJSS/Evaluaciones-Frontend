af449a16046a62cda714fe5b63f77783b0a4a58a
Hugo <peke-vichugo900000@hotmail.com>
2022-07-13 14:55:55 -0500

    Encuesta Satisfaccion

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -3,7 +3,7 @@ function Final() {
   return (
                  <div className="mt-5 md:my-5 shadow-lg px-5 py-1 rounded-xl bg-green-100">
                  <p className="block text-center my-5 text-green-900">
-                 ¡Genial! Tu evaluación se ha guardado y enviado correctamente, mantente al pendiente de las intrucciones de tu asesor.
+                 ¡Genial! se ha guardado y enviado correctamente, mantente al pendiente de las intrucciones de tu asesor.
                  </p>
                </div>
   )
